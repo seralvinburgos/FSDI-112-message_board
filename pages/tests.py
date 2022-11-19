13d870682b0b62024b5fb47637ce650fc7fb2546
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-11-19 13:51:33 -0500

    testing completed. files functional

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class SimplePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_templates(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_home_about_uses_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "pages/about.html")
+
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/about.html")
--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,6 @@ from .views import (
         PostListView, 
         PostDetailView,
         PostCreateView,
-        HomePageView,
-        AboutPageView,
 )
 
 
@@ -12,6 +10,4 @@ urlpatterns = [
     path('', PostListView.as_view(), name='list'),
     path('<int:pk>/', PostDetailView.as_view(), name='detail'),
     path('new/', PostCreateView.as_view(), name='new'),
-    path('home/', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
 ]
